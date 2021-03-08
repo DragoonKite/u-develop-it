a2c8fde83484a55f93ec59facc8464613e434f36
DragoonKite <frank.depaolo@yahoo.com>
2021-03-08 16:26:33 -0600

    add post route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require('./utils/inputCheck');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -57,7 +58,7 @@ app.get('/api/candidates/:id', (req,res) => {
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
-    
+
     db.run(sql, params, function(err, result) {
         if (err) {
             res.status(400).json({ error: res.message });
@@ -70,17 +71,33 @@ app.delete('/api/candidate/:id', (req, res) => {
         });
     });
 });
+
 //Create a candidate
-/* const sql = 'INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES  (?,?,?,?)';
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
 
-const params = [1, 'Roland', 'Firebank', 1];
-
-db.run(sql, params, function(err,result) {
-    if (err) {
-        console.log(err);
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
     }
-    console.log(result, this.lastID);
-}); */
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.run(sql, params, function(err, result) {
+        if (err) {
+          res.status(400).json({ error: err.message });
+          return;
+        }
+      
+        res.json({
+          message: 'success',
+          data: body,
+          id: this.lastID
+        });
+    });
+});
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
